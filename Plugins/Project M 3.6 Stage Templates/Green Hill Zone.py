df0b3e742cce57cffe22afb32bc133496c97a6f4
soopercool101 <soopercoolstages@gmail.com>
2019-11-06 09:51:22 -0800

    Add all stages present as of v0.26b

--- Plugins/Project M 3.6 Stage Templates/Green Hill Zone.py
+++ Plugins/Project M 3.6 Stage Templates/Green Hill Zone.py
@@ -4,4 +4,4 @@ __version__ = "1.0.0"
 from BrawlCrate.API import BrawlAPI
 from System.IO import Path
 
-BrawlAPI.OpenTemplate(Path.Combine(BrawlAPI.PluginPath, "Project M 3.6 Stage Templates", "STGBATTLEFIELD.pac"))
+BrawlAPI.OpenTemplate(Path.Combine(BrawlAPI.PluginPath, "Project M 3.6 Stage Templates", "STGGREENHILL.pac"))
