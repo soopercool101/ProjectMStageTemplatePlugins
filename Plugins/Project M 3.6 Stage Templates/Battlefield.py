18f29beb953e3f0685bb16a7708929a5aa83a387
soopercool101 <soopercoolstages@gmail.com>
2019-11-06 09:23:01 -0800

    Add Battlefield

---
+++ Plugins/Project M 3.6 Stage Templates/Battlefield.py
@@ -0,0 +1,7 @@
+__author__ = "soopercool101 and Sammi Husky"
+__version__ = "1.0.0"
+
+from BrawlCrate.API import *
+from System.IO import Path
+
+BrawlAPI.OpenTemplate(Path.Combine(BrawlAPI.PluginPath, "Project M 3.6 Stage Templates", "STGBATTLEFIELD.pac"))
