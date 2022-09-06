b61ce13bdcde7883bb2146a7509288daeb510d89
soopercool101 <soopercoolstages@gmail.com>
2022-09-06 08:40:40 -0700

    Use Resources path instead of plugin directory to match v0.38 convention

--- Plugins/Project M 3.6 Stage Templates/Green Hill Zone.py
+++ Plugins/Project M 3.6 Stage Templates/Green Hill Zone.py
@@ -1,7 +1,7 @@
 __author__ = "soopercool101"
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 from BrawlCrate.API import BrawlAPI
 from System.IO import Path
 
-BrawlAPI.OpenTemplate(Path.Combine(BrawlAPI.PluginPath, "Project M 3.6 Stage Templates", "STGGREENHILL.pac"))
+BrawlAPI.OpenTemplate(Path.Combine(BrawlAPI.ResourcesPath, "Project M 3.6 Stage Templates", "STGGREENHILL.pac"))
